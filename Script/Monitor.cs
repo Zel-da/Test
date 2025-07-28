935c0e6d9fd2b4aabd9a27c7718122bc803a76bc
YeJoon Ahn <66552780+Zel-da@users.noreply.github.com>
2025-07-28 09:45:52 +0900

    28

---
+++ Script/Monitor.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Monitor : MonoBehaviour
+{
+    //✅
+    [SerializeField] public PlayerController playerController;
+    //필요한 컴포넌트
+    [SerializeField]
+    private GameObject Room1_Monitor;
+    [SerializeField]
+    private GameObject Room1MQ;
+    [SerializeField]
+    private GameObject Crosshair;
+    [SerializeField]
+    private GameObject Text_UI;
+    [SerializeField]
+    public TextMeshProUGUI TextUI;
+
+    // bool값
+    private bool Openscreen = false;
+    public bool IsTouch = false;
+
+    public Door door;
+    public Paper paper;
+    public Case Case;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        TryOpenScreen();
+        CloseText();
+        TryCloseScreen();
+        OpenPaper();
+        TryOpenCase();
+    }
+
+
+    //모니터 보기
+    private void TryOpenScreen()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+        IsTouch = Physics.Raycast(ray, out hit, 0.7f);
+        if (IsTouch)
+        {
+            if (hit.transform.CompareTag("PC"))
+            {
+                TextUI.text = " 화면을 보려면 (Z)키를 누르세요";
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    playerController.SetCanMove(false);  // 이동 금지
+                    Text_UI.SetActive(false);
+                    Crosshair.SetActive(false);
+                    Room1MQ.SetActive(true);
+                    Openscreen = true;
+                }
+
+
+            }
+        }
+    }
+
+    //모니터 닫기
+    private void TryCloseScreen()
+    {
+        if (Openscreen == true)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                playerController.SetCanMove(true); //이동 허용
+                Text_UI.SetActive(true);
+                Crosshair.SetActive(true);
+                Room1MQ.SetActive(false);
+                Openscreen = false;
+            }
+        }
+
+    }
+
+    //상호작용키 지우기
+    public void CloseText()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+        IsTouch = Physics.Raycast(ray, out hit, 0.7f);
+        if (IsTouch)
+        {
+            if (!hit.transform.CompareTag("PC") && !hit.transform.CompareTag("Door") && !hit.transform.CompareTag("Paper") && !hit.transform.CompareTag("Case"))
+            {
+                TextUI.text = " ";
+            }
+        }
+        else
+        {
+            TextUI.text = "";
+        }
+    }
+
+    //종이보는 상호작용키 띄우기
+    private void OpenPaper()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+        IsTouch = Physics.Raycast(ray, out hit, 0.7f);
+        if (IsTouch)
+        {
+            if (hit.transform.CompareTag("Paper"))
+            {
+                TextUI.text = "종이를 보려면 (Z)키를 누르세요";
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    playerController.SetCanMove(false);  // 이동 금지
+                    paper.OpenPaper();
+                    Text_UI.SetActive(false);
+                    Crosshair.SetActive(false);
+                }
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    playerController.SetCanMove(true);  // 이동 허용
+                    Text_UI.SetActive(true);
+                    Crosshair.SetActive(true);
+                }
+
+            }
+        }
+    }
+
+    //사물함 열기
+    private void TryOpenCase()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        Debug.DrawRay(ray.origin, ray.direction * 5f, Color.yellow);
+        RaycastHit hit;
+        IsTouch = Physics.Raycast(ray, out hit, 0.7f);
+        if (IsTouch)
+        {
+            if (hit.transform.CompareTag("Case"))
+            {
+                TextUI.text = "서랍을 열려면 (Z)키를 누르세요";
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    playerController.SetCanMove(false);  // 이동 금지
+                    Case.OpenCase();
+                    Text_UI.SetActive(false);
+                    Crosshair.SetActive(false);
+                }
+
+            }
+        }
+    }
+}
---
+++ Script/PlayerController.cs
@@ -0,0 +1,292 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //✅
+
+    [SerializeField]
+    private float walkSpeed;
+    [SerializeField]
+    private float runSpeed;
+    [SerializeField]
+    private float crouchSpeed;
+
+    private float applySpeed;
+
+    [SerializeField]
+    private float jumpForce;
+
+    // 움직임 가능?
+    private bool isMoveable = true;
+
+
+    private bool isWalk = false;
+    private bool isRun = false;
+    private bool isCrouch = false;
+    private bool isGround = true;
+
+
+    private Vector3 lastPos;
+
+
+    [SerializeField]
+    private float crouchPosY;
+    private float originPosY;
+    private float applyCrouchPosY;
+
+
+    private CapsuleCollider capsuleCollider;
+
+
+
+    [SerializeField]
+    private float lookSensitivity;
+
+
+    [SerializeField]
+    private float cameraRotationLimit;
+    private float currentCameraRotationX = 0;
+
+
+
+    [SerializeField]
+    private Camera theCamera;
+    private Rigidbody myRigid;
+
+    void Start()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        myRigid = GetComponent<Rigidbody>();
+
+        // �ʱ�ȭ
+        applySpeed = walkSpeed;
+        originPosY = theCamera.transform.localPosition.y;
+        applyCrouchPosY = originPosY;
+    }
+    void Update()
+    {
+        if (!isMoveable) return;
+
+        IsGround();
+        TryJump();
+        TryRun();
+        TryCrouch();
+        Move();
+        MoveCheck();
+        CameraRotation();
+        CharacterRotation();
+    }
+
+    public void SetCanMove(bool canMove)
+    {
+        isMoveable = canMove;
+    }
+
+    public bool GetCanMove()
+    {
+        return isMoveable;
+    }
+
+    // �ɱ� �õ�
+    private void TryCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+
+
+    // �ɱ� ����
+    private void Crouch()
+    {
+        isCrouch = !isCrouch;
+
+        if (isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+        else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+
+        StartCoroutine(CrouchCoroutine());
+
+    }
+
+
+    IEnumerator CrouchCoroutine()
+    {
+
+        float _posY = theCamera.transform.localPosition.y;
+        int count = 0;
+
+        while (_posY != applyCrouchPosY)
+        {
+            count++;
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.3f);
+            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
+            if (count > 15)
+                break;
+            yield return null;
+        }
+        theCamera.transform.localPosition = new Vector3(0, applyCrouchPosY, 0f);
+    }
+
+
+
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+
+
+    private void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+
+
+    private void Jump()
+    {
+
+        if (isCrouch)
+            Crouch();
+        myRigid.linearVelocity = transform.up * jumpForce;
+    }
+
+
+    private void TryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+
+    // �޸��� ����
+    private void Running()
+    {
+        if (isCrouch)
+            Crouch();
+
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+
+    // �޸��� ���
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
+
+
+    // ������ ����
+    private void Move()
+    {
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _moveDirX;
+        Vector3 _moveVertical = transform.forward * _moveDirZ;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
+
+        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+    }
+
+
+    // ������ üũ
+    private void MoveCheck()
+    {
+        if (!isRun && !isCrouch && isGround)
+        {
+            if (Vector3.Distance(lastPos, transform.position) >= 0.01f)
+                isWalk = true;
+            else
+                isWalk = false;
+            lastPos = transform.position;
+        }
+    }
+
+
+    // �¿� ĳ���� ȸ��
+    private void CharacterRotation()
+    {
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
+    }
+
+
+
+    // ���� ī�޶� ȸ��
+    private void CameraRotation()
+    {
+        if (!pauseCameraRotation)
+        {
+            float _xRotation = Input.GetAxisRaw("Mouse Y");
+            float _cameraRotationX = _xRotation * lookSensitivity;
+            currentCameraRotationX -= _cameraRotationX;
+            currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+            theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+        }
+    }
+
+    private bool pauseCameraRotation = false;
+
+    public IEnumerator TreeLookCoroutine(Vector3 _target)
+    {
+        pauseCameraRotation = true;
+
+        Quaternion direction = Quaternion.LookRotation(_target - theCamera.transform.position);
+        Vector3 eulerValue = direction.eulerAngles;
+        float destinationX = eulerValue.x;
+
+        while (Mathf.Abs(destinationX - currentCameraRotationX) >= 0.5f)
+        {
+            eulerValue = Quaternion.Lerp(theCamera.transform.localRotation, direction, 0.3f).eulerAngles;
+            theCamera.transform.localRotation = Quaternion.Euler(eulerValue.x, 0f, 0f);
+            currentCameraRotationX = theCamera.transform.localEulerAngles.x;
+            yield return null;
+        }
+
+        pauseCameraRotation = false;
+    }
+
+    // ���� ���� �� ��ȯ
+    public bool GetRun()
+    {
+        return isRun;
+    }
+    public bool GetWalk()
+    {
+        return isWalk;
+    }
+    public bool GetCrouch()
+    {
+        return isCrouch;
+    }
+    public bool GetIsGround()
+    {
+        return isGround;
+    }
+}
